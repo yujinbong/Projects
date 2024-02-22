9966bde6610713201545e882e9ead9e7c2526b4b
Yujin <84473069+yujinbong@users.noreply.github.com>
2024-02-21 23:28:52 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitPay
+{
+    internal class Program
+    {
+        class Game
+        {
+            private List<string> players;
+            public string Player { get; set; }
+            public int Ladder { get; set; }
+            public string LadderStatement { get; set; }
+
+            public void StartGame()
+            {
+                Console.WriteLine("Please enter the names of the players: ");
+
+                Player = new List
+            }
+
+
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
